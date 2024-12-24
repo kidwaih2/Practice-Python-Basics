240386db4a006a53a16d2ed536b4314422da10d2
Hashimk2 <hashimkidwai@gmail.com>
2024-12-24 13:07:25 -0600

    Add L5_Formatting_Numbers notes/code

---
+++ Week 1/L5_Formatting_Numbers.py
@@ -0,0 +1,6 @@
+number_1 = '23'
+number_2 = '32'
+number = 9/2
+print('First number: {0:.1f}'.format(number))
+print('Second number: {0:.1f}'.format(number))
+print('Your sum:', int(number_1) + int(number_2))
